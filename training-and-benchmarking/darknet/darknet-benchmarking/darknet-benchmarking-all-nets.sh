21f43946c785160ef57ce22f489c7a42c9ec4798
solder-fumes-asthma <63670587+solder-fumes-asthma@users.noreply.github.com>
2021-04-07 15:16:48 +0200

    added comments

--- training-and-benchmarking/darknet/darknet-benchmarking/darknet-benchmarking-all-nets.sh
+++ training-and-benchmarking/darknet/darknet-benchmarking/darknet-benchmarking-all-nets.sh
@@ -1,8 +1,13 @@
-python3 test-set-resizer.py ~/YOLOv4/PPU-6/test/ 416 512 640
+# Full benchmarking of MAP and FPS for various darknet models
 
+# Generate a resized copy of the test set for benchmarking the nets on their baseline resolution
+test-set-resizer.py ~/YOLOv4/PPU-6/test/ 416 512 640
+
+# Temporary cd to darknet dir
 pushd ~/YOLOv4/darknet
 
-# TINY
+
+# Darknet inference (results to text file) on TINY
 LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
     ./darknet detector test \
         ~/YOLOv4/PPU-6/obj.data \
@@ -11,7 +16,7 @@ LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
         -dont_show -ext_output < $OLDPWD/test-416.txt \
         > $OLDPWD/results-yolov4-tiny-416.txt
 
-# TINY 3L
+# Darknet inference (results to text file) on TINY 3L
 LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
     ./darknet detector test \
         ~/YOLOv4/PPU-6/obj.data \
@@ -20,7 +25,7 @@ LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
         -dont_show -ext_output < $OLDPWD/test-416.txt \
         > $OLDPWD/results-yolov4-tiny-3l-416.txt
 
-# YOLOv4
+# Darknet inference (results to text file) on YOLOv4
 LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
     ./darknet detector test \
         ~/YOLOv4/PPU-6/obj.data \
@@ -29,7 +34,7 @@ LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
         -dont_show -ext_output < $OLDPWD/test-416.txt \
         > $OLDPWD/results-yolov4-416.txt
 
-# YOLOv4 CSP
+# Darknet inference (results to text file) on YOLOv4 CSP
 LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
     ./darknet detector test \
         ~/YOLOv4/PPU-6/obj.data \
@@ -38,7 +43,7 @@ LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
         -dont_show -ext_output < $OLDPWD/test-512.txt \
         > $OLDPWD/results-yolov4-csp-512.txt
 
-# YOLOv4 MISH
+# Darknet inference (results to text file) on YOLOv4 MISH
 LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
     ./darknet detector test \
         ~/YOLOv4/PPU-6/obj.data \
@@ -47,10 +52,13 @@ LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/cuda-10.2/targets/x86_64-linux/lib/ \
         -dont_show -ext_output < $OLDPWD/test-640.txt \
         > $OLDPWD/results-yolov4x-mish-640.txt
 
+
+# Pop back to previous dir (the one holding this script)
 popd
 
 
-
+# Convert inference results from darknet to MS COCO
+# PPU-6-size.json is loaded only to provide the paths to the images
 python3 darknet_2_coco_detections.py \
     ./assets/PPU-6-416.json \
     results-yolov4-tiny-416.txt \
@@ -77,7 +85,7 @@ python3 darknet_2_coco_detections.py \
     results-yolov4x-mish-640.json
 
 
-
+# Convert inference results from darknet to MS COCO
 python3 batch-annotate.py \
     results-yolov4-tiny-416.txt \
     results-yolov4-tiny-3l-416.txt \
@@ -86,16 +94,17 @@ python3 batch-annotate.py \
     results-yolov4x-mish-640.txt
 
 
+# Dump new files into dump dir
 rm -r ./generated-files-dump
 mkdir ./generated-files-dump
 mv *.json ./generated-files-dump
 mv *.txt ./generated-files-dump
 mv resized-* ./generated-files-dump
-
 mkdir ./infer_results
 mv results* ./infer_results
 
 
+# Remove MAP-table if exists so it is overwritten & generate MAP table text file
 rm MAP-table.txt
 
 echo MAP TABLE FOR YOLOv4-TINY >> MAP-table.txt
@@ -129,8 +138,7 @@ python3 valcoco.py \
     >> MAP-table.txt
 
 
-
-
+# Get the average FPS from the inference logs
 python3 fps-retriever.py \
     FPS.txt \
     ./generated-files-dump/results-yolov4-tiny-416.txt \
@@ -140,4 +148,5 @@ python3 fps-retriever.py \
     ./generated-files-dump/results-yolov4x-mish-640.txt
 
 
+# Finish message alert
 echo Finished benchmarking successfully!
