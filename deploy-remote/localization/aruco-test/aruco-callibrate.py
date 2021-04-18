411ae3f4498a0189d68d6e84872e3d65ef014f4c
solder-fumes-asthma <63670587+solder-fumes-asthma@users.noreply.github.com>
2021-04-18 16:12:51 +0200

    Add files via upload

--- deploy-remote/localization/aruco-test/aruco-callibrate.py
+++ deploy-remote/localization/aruco-test/aruco-callibrate.py
@@ -8,7 +8,7 @@ REVISION PENDING
 
 This script is used to record a video of a calibration pattern, that is also
 generated within (and you must print), so that the aruco callibration function
-will output the camera matrix and distance vector. 
+will output the camera matrix and distance vector.
 '''
 
 
@@ -17,18 +17,18 @@ board = cv2.aruco.CharucoBoard_create(3,3,.025,.0125,dictionary)
 img = board.draw((200*3,200*3))
 
 #Dump the calibration board to a file
-cv2.imwrite('charuco.png',img)
+cv2.imwrite('charuco-calibration-pattern.png',img)
 
 # Quit to just print out the example file for calibration
 # quit()
 
 #Start capturing images for calibration
-cap = cv2.VideoCapture('https://130.240.152.20:8085/video')
+cap = cv2.VideoCapture('https://192.168.0.107:8085/video')
 
 allCorners = []
 allIds = []
 decimator = 0
-for i in range(300):
+for i in range(500):
 
     ret,frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -42,7 +42,7 @@ for i in range(300):
 
         cv2.aruco.drawDetectedMarkers(gray,res[0],res[1])
 
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',cv2.pyrDown(gray))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     decimator+=1
@@ -51,10 +51,17 @@ imsize = gray.shape
 
 #Calibration fails for lots of reasons. Release the video if we do
 try:
-    cal = cv2.aruco.calibrateCameraCharuco(allCorners,allIds,board,imsize,None,None)
-    print(cal)
+    print('Callibration started!')
+    retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(allCorners,allIds,board,imsize,None,None)
+    print('Saving calibration results to file...', end ='')
+    np.savez('RedmiNote9Pro', retval=retval, cameraMatrix=cameraMatrix, distCoeffs=distCoeffs, rvecs=rvecs, tvecs=tvecs)
+    print(' Saved!')
+    print(cameraMatrix)
+    print(distCoeffs)
 except:
+    print('FAILED CALIBRATION!')
     cap.release()
+
 
 cap.release()
 cv2.destroyAllWindows()
